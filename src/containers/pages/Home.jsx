9941fe575000ac5f82b36e17e6d61ef6150fc3cd
Jheanmes <138171228+milder882@users.noreply.github.com>
2024-11-08 02:29:11 -0500

    ml

---
+++ src/containers/pages/Home.jsx
@@ -0,0 +1,15 @@
+
+import FullWidthLayout from 'hocs/layouts/FullWidthLayout'
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <FullWidthLayout>
+        home
+      </FullWidthLayout>
+    </div>
+  )
+}
+
+export default Home
